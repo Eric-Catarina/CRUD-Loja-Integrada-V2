a1a6f4a4a0e6a240350449f2d0aee7edb0132461
Eric-Catarina <2020954200@teiacoltec.org>
2022-07-22 14:16:30 -0300

    Primeiro commit

---
+++ admin.js
@@ -0,0 +1,21 @@
+
+// ============================================
+// Admin Bro
+const AdminBro = require('admin-bro')
+const AdminBroExpress = require('admin-bro-expressjs')
+
+const adminBro = new AdminBro({
+  databases: [],
+  rootPath: '/admin',
+})
+
+const router = AdminBroExpress.buildRouter(adminBro)
+
+// ============================================
+// Server
+const express = require("express");
+const server = express();
+
+server
+  .use(adminBro.options.rootPath, router)
+  .listen(5500, () => console.log("Server started"));
